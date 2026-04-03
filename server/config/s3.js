848eee3d9976593a19946576c6de8f563b1dd12b
Sonu <axs.sh7@gmail.com>
2026-04-03 11:24:44 +0530

    fix: s3-configuration for env issue

--- server/config/s3.js
+++ server/config/s3.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const s3 = new S3Client({
-  region: process.env.AWS_REGION.trim(),
+  region:  process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID.trim(),
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY.trim(),
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 
 });
